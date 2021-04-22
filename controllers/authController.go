32ebfb9b24f0204a73cd9a01e14220546424d7a7
mikusher <mikusher@hotmail.com>
2021-04-22 17:51:45 +0100

    Login controller and user check status

--- controllers/authController.go
+++ controllers/authController.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"go-admin/database"
 	"go-admin/models"
-	"golang.org/x/crypto/bcrypt"
+	"go-admin/utils"
 )
 
 func Register(c *fiber.Ctx) error {
@@ -21,17 +21,47 @@ func Register(c *fiber.Ctx) error {
 		})
 	}
 
-	password, _ := bcrypt.GenerateFromPassword([]byte(data["Password"]), 14)
+	hashedPassword := utils.HashPassword(data["password"])
 
 	user := models.User{
 		FirstName: data["first_name"],
 		LastName:  data["last_name"],
 		Email:     data["email"],
-		Password:  password,
+		Password:  hashedPassword,
 	}
 
 	//save on database
 	database.DB.Create(&user)
 
 	return c.JSON(user)
+}
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	var user models.User
+
+	//get user by email
+	database.DB.Where("email = ?", data["email"]).First(&user)
+
+	if user.Id == 0 {
+		c.Status(404)
+		return c.JSON(fiber.Map{
+			"message": "User not found",
+		})
+	}
+
+	if utils.CheckPasswordHash(data["password"], string(user.Password)) {
+		return c.JSON(user)
+	} else {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "Incorrect Password",
+		})
+	}
+
 }
---
+++ utils/util.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) []byte {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+	return []byte(bytes)
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
