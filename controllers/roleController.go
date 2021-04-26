04be6e52fb08130f0d1570c62e8ff7607b93abdd
Luis Tavares <mikusher@hotmail.com>
2021-04-26 14:13:50 +0100

    role configuration

---
+++ controllers/roleController.go
@@ -0,0 +1,61 @@
+package controllers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"go-admin/database"
+	"go-admin/models"
+	"strconv"
+)
+
+func AllRoles(c *fiber.Ctx) error {
+	var roles []models.Role
+
+	database.DB.Find(&roles)
+	return c.JSON(roles)
+}
+
+func CreateRole(c *fiber.Ctx) error {
+	var role models.Role
+
+	if err := c.BodyParser(&role); err != nil {
+		return err
+	}
+
+	//create a new user in the database
+	database.DB.Create(&role)
+
+	return c.JSON(role)
+}
+
+func GetRole(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+	role := models.Role{
+		Id: uint(id),
+	}
+	database.DB.Find(&role)
+	return c.JSON(role)
+}
+
+func UpdateRole(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+	role := models.Role{
+		Id: uint(id),
+	}
+
+	if err := c.BodyParser(&role); err != nil {
+		return err
+	}
+	database.DB.Model(&role).Updates(role)
+	return c.JSON(role)
+}
+
+func DeleteRole(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+	role := models.Role{
+		Id: uint(id),
+	}
+	database.DB.Delete(&role)
+	return c.JSON(fiber.Map{
+		"message": "role deleted",
+	})
+}
